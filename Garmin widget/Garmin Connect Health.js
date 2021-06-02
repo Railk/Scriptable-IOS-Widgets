e1e17085e5cf51b5edc3e4023bff153bdb49d278
Railk <thinkprocess@yahoo.fr>
2021-06-02 11:41:19 +0200

    Update Garmin Connect Health.js
    Use signin url to login

--- Garmin widget/Garmin Connect Health.js
+++ Garmin widget/Garmin Connect Health.js
@@ -28,7 +28,7 @@ const GC_MODERN = 'https://connect.garmin.com/modern';
 const GARMIN_SSO = 'https://sso.garmin.com/sso';
 const BASE_URL = GC_MODERN+'/proxy';
 const SIGNIN_URL = GARMIN_SSO+'/signin';
-const LOGIN_URL = GARMIN_SSO+'/login';
+//const LOGIN_URL = GARMIN_SSO+'/login';
 
 const ACTIVITY_SERVICE = BASE_URL+'/activity-service';
 const ACTIVITYLIST_SERVICE = BASE_URL+'/activitylist-service';
@@ -187,7 +187,7 @@ async function signin(){
 }
 
 async function login(){
-  let req = postRequest(LOGIN_URL,CREDENTIALS,getQuery(PARAMS));
+  let req = postRequest(SIGNIN_URL,CREDENTIALS,getQuery(PARAMS));
   await req.load();
   let res = req.response;
   parseCookie(res.cookies);
